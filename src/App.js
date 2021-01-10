9df46c38b4edb862d4ac2ef5aeb252e1d7fede1a
orlando <ing97220@naver.com>
2021-01-10 18:43:41 +0900

    2-3

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
 import React from 'react';
 
-function Food({name, picture}){
-  return (
-  <div>
-  <h1>I like {name}</h1>
-  <img src = {picture} />;
-  </div>
-  )
-}
-
 //동적 호출
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
     "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.EQfyKbJaFRSaXJvDxMr6mgHaHa%26pid%3DApi&f=1"
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
     "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.o1b4qpMJCb-Z0yHM7vSgMwHaHa%26pid%3DApi&f=1"
   },
   {
+    id: 3,
     name: "Bibimbap",
     image:
     "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.B3ddBez0ci1Fga_TDPmFhwHaEK%26pid%3DApi&f=1"
   }
 ];
 
-function App() {
+function Food({name, picture}){
   return (
   <div>
+  <h1>I like {name}</h1>
+  <img src = {picture} alt = {name} />;
+  </div>
+  )
+}
+
+//함수를 통한 렌더링
+function renderFood(dish){
+  console.log(dish);
+    return (<Food name = {dish.name} picture = {dish.image} />)
+}
+
+function App() {
+  return (
+  <div>    
+    {console.log(foodILike.map(renderFood))}    
+    {foodILike.map(renderFood)}
+
     {foodILike.map(dish => (
-      <Food name = {dish.name} picture = {dish.image} /> 
-    ))} 
+      <Food key = {dish.id}  name = {dish.name} picture={dish.image} />
+    ))}
   </div>
   );
 }
