c72e6d7afa877708e69b1258255f614ba6826550
orlando <ing97220@naver.com>
2021-01-10 18:25:08 +0900

    동적 처리

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React from 'react';
 
-function Food({fav}){
-  return <h1>I like {fav}</h1>;
+function Food({name, picture}){
+  return (
+  <div>
+  <h1>I like {name}</h1>
+  <img src = {picture} />;
+  </div>
+  )
 }
 
+//동적 호출
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.EQfyKbJaFRSaXJvDxMr6mgHaHa%26pid%3DApi&f=1"
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.o1b4qpMJCb-Z0yHM7vSgMwHaHa%26pid%3DApi&f=1"
+  },
+  {
+    name: "Bibimbap",
+    image:
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.B3ddBez0ci1Fga_TDPmFhwHaEK%26pid%3DApi&f=1"
+  }
+];
+
 function App() {
-  return <div>
-    <h1>Hello</h1>
-    <Food fav = "Kimchi"/>
-    <Food fav = "Ramen"/>
-    <Food fav = "Samgiopsal"/>
-    <Food fav = "Chukumi"/>
-    </div>;
+  return (
+  <div>
+    {foodILike.map(dish => (
+      <Food name = {dish.name} picture = {dish.image} /> 
+    ))} 
+  </div>
+  );
 }
 
 export default App;   
